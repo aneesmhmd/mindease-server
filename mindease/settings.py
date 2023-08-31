2a59eedac46b009575ecfdad2017382e896993c3
aneesmhmd <aneesanz221@gmail.com>
2023-08-31 15:24:03 +0530

    origins updation

--- mindease/settings.py
+++ mindease/settings.py
@@ -197,35 +197,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
+    'https://mindeaseonline.vercel.app',
     config('origin_one'),
     config('origin_two'),
-]
-
-CORS_ORIGIN_WHITELIST = [
-    config('origin_one'),
-    config('origin_two'),
-]
-
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-]
-
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-    'Authorization',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
