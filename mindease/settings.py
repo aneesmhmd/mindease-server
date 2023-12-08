4bcb5c910f70baa9ef7bedb2333add93049d8029
aneesmhmd <aneesanz221@gmail.com>
2023-12-08 09:55:23 +0530

    db setup

--- mindease/settings.py
+++ mindease/settings.py
@@ -143,14 +143,11 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('DB_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
